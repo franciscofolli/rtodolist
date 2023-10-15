1a5cff89a8713a3c3d32cc38f5d988ffa9684cf1
franciscofolli <franciscoabel2001@gmail.com>
2023-10-15 08:21:48 -0300

    Subindo estrutura initialzr do Spring com java 17

---
+++ src/main/java/br/com/franciscofolli/rtodolist/RtodolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.franciscofolli.rtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/franciscofolli/rtodolist/RtodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.franciscofolli.rtodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
